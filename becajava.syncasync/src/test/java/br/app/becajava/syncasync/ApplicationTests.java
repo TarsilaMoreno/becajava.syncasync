ab850a20992ab98f48ff8b0c8ab775c26f8d1c7b
Tarsila Muriel Moreno <tmurielm@everis.com>
2020-11-17 17:02:13 -0300

    first commit

---
+++ becajava.syncasync/src/test/java/br/app/becajava/syncasync/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.app.becajava.syncasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
